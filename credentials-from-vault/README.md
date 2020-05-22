be76e8da2ebf6f4b056354f57929db588ea016fb
David Thor <david.thor@arcussolutions.com>
2020-05-22 10:14:58 -0400

    Got vault integration working

---
+++ credentials-from-vault/README.md
@@ -0,0 +1,2 @@
+# Managing secrets with Hashicorp Vault
+
--- credentials-from-vault/index.js
+++ credentials-from-vault/index.js
@@ -7,25 +7,32 @@ const vault_client = VaultSDK({
   token: process.env.VAULT_TOKEN // optional client token; can be fetched after valid initialization of the server
 });
 
+const readVaultSecret = async (secret) => {
+  const prefix = secret.substring(0, secret.lastIndexOf('/'));
+  const name = secret.substring(secret.lastIndexOf('/') + 1, secret.indexOf('#') >= 0 ? secret.indexOf('#') : undefined);
+  const key = secret.substring(secret.indexOf('#') + 1);
+  const res = await vault_client.read(`${prefix}/data/${name}`);
+  return res.data.data[key];
+};
+
 const run = async () => {
-  console.log(process.env.DATABASE_USER_SECRET.split('#')[0]);
   const postgres_config = {
     host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    user: await vault_client.read(process.env.DATABASE_USER_SECRET.split('#')[0]),
-    password: await vault_client.read(process.env.DATABASE_PASSWORD_SECRET.split('#')[0]),
-    database: await vault_client.read(process.env.DATABASE_NAME_SECRET.split('#')[0]),
+    port: Number(process.env.DATABASE_PORT),
+    user: await readVaultSecret(process.env.DATABASE_USER_SECRET),
+    password: await readVaultSecret(process.env.DATABASE_PASSWORD_SECRET),
+    database: await readVaultSecret(process.env.DATABASE_NAME_SECRET),
   };
   const postgres_client = new PostgresClient(postgres_config);
   postgres_client.connect();
 
   // Create and seed table
-  await postgres_client.query('CREATE TABLE users (id int, last_name varchar(255), first_name varchar(255)');
-  await postgres_client.query('INSERT INTO users (id, last_name, first_name) VALUES (1, "User", "Test")');
+  await postgres_client.query('CREATE TABLE users (id int, last_name varchar(255), first_name varchar(255))');
+  await postgres_client.query('INSERT INTO users (id, last_name, first_name) VALUES (1, \'User\', \'Test\')');
 
   // Query seeded content
   const result = await postgres_client.query('SELECT * FROM users');
-  console.log(result);
+  console.log(result.rows);
 
   // Drop table and kill connection
   await postgres_client.query('DROP TABLE users');
